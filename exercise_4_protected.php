5d94ba29dfdaee0f2f2aa33c6d34c6e9e94b1400
Wendy Quarteer <wendyquarteer@outlook.com>
2022-06-24 11:09:31 +0200

    ex 4 - protected

--- exercise_4_protected.php
+++ exercise_4_protected.php
@@ -4,10 +4,90 @@ declare(strict_types=1);
 
 /* EXERCISE 4
 
-Copy the code of exercise 2 to here and delete everything related to cola.
+Copy the code of exercise 3 to here and delete everything related to cola.
 
 TODO: Make all properties protected.
 TODO: Make all the other prints work without error without changing the beverage class.
 
 USE TYPEHINTING EVERYWHERE!
 */
+
+class Beverage {
+    protected string $color;
+    protected float $price;
+    protected string $temperature;
+
+    /**
+     * @param string $color
+     * @param float|int $price
+     */
+    public function __construct(string $color, float $price)
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = "cold";
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function getInfo(): void
+    {
+        echo "This beverage is $this->temperature and $this->color";
+    }
+}
+
+class Beer extends Beverage {
+    protected string $name;
+    protected float $alcoholPercentage;
+
+    /**
+     * @param string $name
+     * @param float $alcoholPercentage
+     */
+    public function __construct(string $name, float $alcoholPercentage, string $color, float $price)
+    {
+        parent:: __construct($color, $price);
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAlcoholPercentage(): float
+    {
+        return $this->alcoholPercentage;
+    }
+
+    /**
+     * @param string $color
+     */
+    public function setColor(string $color)
+    {
+        $this->color = $color;
+    }
+
+    public function beerInfo(): void
+    {
+        echo "Hi i'm $this->name and have an alcohol percentage of $this->alcoholPercentage and I have a $this->color color.";
+    }
+}
+$duvel = new Beer("Duvel", 8.5, "blond", 3.5);
+echo "<br>";
+echo $duvel->getAlcoholPercentage();
+echo "<br>";
+echo $duvel->getAlcoholPercentage();
+echo "<br>";
+echo $duvel->getColor();
+echo "<br>";
+$duvel->getInfo();
+echo "<br>";
+echo $duvel->setColor("Light");
+echo "<br>";
+echo $duvel->beerInfo();
